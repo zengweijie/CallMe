fd632b669def56a530294622de7567fbdea7cde3
zengweijie <2063627322@qq.com>
2017-04-11 10:14:47 +0800

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/callme/CallMeActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.callme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CallMeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call_me);
+    }
+}
